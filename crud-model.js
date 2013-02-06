a6cf69d36ddd0469bc6fc0cc62737dda6471b1e6
Paul Serby <paul.serby@clock.co.uk>
2013-02-06 15:46:05 +0000

    ValidateOptions is now optional

--- crud-model.js
+++ crud-model.js
@@ -31,6 +31,11 @@ module.exports = function(name, save, schema, options) {
     schema: schema,
     idProperty: save.idProperty,
     create: function(object, validateOptions, callback) {
+
+      if (typeof validateOptions === 'function') {
+        callback = validateOptions
+      }
+
       callback = callback || emptyFn
 
       var cleanObject = schema.cast(schema.stripUnknownProperties(schema.makeDefault(object), validateOptions.tag))
